b21597c4d7f919a3e77c0297ce3bf5ee3d52302e
Brandon <brandon.shian@gmail.com>
2017-11-03 03:55:53 +0800

    first commit

---
+++ homework_1/Controllers/客戶聯絡人Controller.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using homework_1.Models;
+
+namespace homework_1.Controllers
+{
+    public class 客戶聯絡人Controller : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
+        // GET: 客戶聯絡人
+        public ActionResult Index()
+        {
+            var 客戶聯絡人 = db.客戶聯絡人.Include(客 => 客.客戶資料);
+            return View(客戶聯絡人.ToList());
+        }
+
+        public ActionResult Search(string ContactInfo)
+        {
+            if (!string.IsNullOrEmpty(ContactInfo))
+            {
+                var data = db.客戶聯絡人.Where(p => p.姓名.Contains(ContactInfo)).ToList();
+                if (data.Count > 0)
+                {
+                    return View("Index", data);
+                }
+            }
+            return View("Index", db.客戶銀行資訊.Take(0).ToList());
+        }
+
+        public ActionResult CheckEmailByCustomerId(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // GET: 客戶聯絡人/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶聯絡人);
+        }
+
+        // GET: 客戶聯絡人/Create
+        public ActionResult Create()
+        {
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱");
+            return View();
+        }
+
+        // POST: 客戶聯絡人/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,客戶Id,職稱,姓名,Email,手機,電話")] 客戶聯絡人 客戶聯絡人)
+        {
+            if (db.客戶聯絡人.Any(p => p.客戶Id == 客戶聯絡人.客戶Id && p.Email == 客戶聯絡人.Email))
+            {
+                ModelState.AddModelError("Email", "同一個客戶下的聯絡人，其 Email 不能重複。");
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.客戶聯絡人.Add(客戶聯絡人);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // GET: 客戶聯絡人/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // POST: 客戶聯絡人/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,客戶Id,職稱,姓名,Email,手機,電話")] 客戶聯絡人 客戶聯絡人)
+        {
+            if (db.客戶聯絡人.Any(p => p.客戶Id == 客戶聯絡人.客戶Id && p.Email == 客戶聯絡人.Email))
+            {
+                ModelState.AddModelError("Email", "同一個客戶下的聯絡人，其 Email 不能重複。");
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(客戶聯絡人).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶聯絡人.客戶Id);
+            return View(客戶聯絡人);
+        }
+
+        // GET: 客戶聯絡人/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+            if (客戶聯絡人 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶聯絡人);
+        }
+
+        // POST: 客戶聯絡人/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶聯絡人 客戶聯絡人 = db.客戶聯絡人.Find(id);
+            db.客戶聯絡人.Remove(客戶聯絡人);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
